03942ef68e39e906357075c20357b5bee5cf2578
tsm <161220117@smail.nju.edu.cn>
2019-04-20 02:10:22 +0800

    start lab2

---
+++ Code/symbol_table.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "syntax.tab.h"
+
+typedef struct Type_* Type;
+typedef struct FieldList_* FieldList;
+typedef enum idkind {_VAR, _STRUCT} IDKind;
+struct Type_ {
+	enum { BASIC, ARRAY, STRUCTURE } kind;
+	union {
+		// 基本类型
+		int basic;	//int = 1; float = 0
+		// 数组类型信息包括元素类型与数组大小构成
+		struct { 
+			Type elem; int size; 
+		} array;
+		// 结构体类型信息是一个链表
+		FieldList structure;
+	} u;
+};
+struct FieldList_ {
+	char name[55]; // 域的名字
+	Type type; // 域的类型
+	FieldList tail; // 下一个域
+};
+
+struct SymbolEntity_ {
+	IDkind idkind;
+	char name[55];
+	
+	Type type;	
+};
+struct SymbolEntityFunc_ {
+	
+} 
